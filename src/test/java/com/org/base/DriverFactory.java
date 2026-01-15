9939050ce02cbec754a0ef1f83afd823323996ab
Mukesh Kumar <mukeshrajput2402@gmail.com>
2026-01-15 12:02:51 +0530

    headless commit

--- src/test/java/com/org/base/DriverFactory.java
+++ src/test/java/com/org/base/DriverFactory.java
@@ -2,6 +2,7 @@ package com.org.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
@@ -9,7 +10,11 @@ public class DriverFactory {
 
     public static WebDriver initDriver() {
 //        WebDriverManager.chromedriver().setup();
-        driver.set(new ChromeDriver());
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        driver.set(new ChromeDriver(options));
         getDriver().manage().window().maximize();
         return getDriver();
     }
